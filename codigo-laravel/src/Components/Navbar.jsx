6df9db3a4d990d3ccb8b42e47f9d0182c4d878ef
JefreeMendoza <142171894+JefreeMendoza@users.noreply.github.com>
2024-06-02 18:25:55 -0500

    algunas actualizaciones

--- codigo-laravel/src/Components/Navbar.jsx
+++ codigo-laravel/src/Components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link , useLocation } from 'react-router-dom'
 import '../style/style-NavBar.css'
 
 function Navbar() {
+    const location = useLocation();
+
+    const getLinkClass = (path) => {
+        return location.pathname === path ? 'navbar-link active' : 'navbar-link';
+    };
+
     return (  
         <>
             <div>
                 <nav className="navbar">
                     {/* <h2 className="navbar-title">Videoclub</h2> */}
                     <ul className="navbar-links">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/nosotros">Nosotros</Link></li>
-                        <li><Link to="/servicios">Servicios</Link></li>
-                        <li><Link to="/contactos">Contactos</Link></li>
-                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/" className={getLinkClass('/')}>Home</Link></li>
+                        <li><Link to="/nosotros" className={getLinkClass('/nosotros')}>Nosotros</Link></li>
+                        <li><Link to="/servicios" className={getLinkClass('/servicios')}>Servicios</Link></li>
+                        <li><Link to="/contactos" className={getLinkClass('/contactos')}>Contactos</Link></li>
+                        <li><Link to="/login" className={getLinkClass('/login')}>Login</Link></li>
                     </ul>
                 </nav>
                 {/* <div className="dashboard-content">
